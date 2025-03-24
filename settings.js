b3172ec722158fa5145ae13fe7e98d9db1b46894
xchamo234 <xchamonirmal9@gmail.com>
2025-03-24 21:07:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,8 +6,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=e2IHwIaA#LqZVkP__qqD112wK6cJ1XpS3CPkwRasA8x2P25ExF0E' : process.env.SESSION_ID,
-PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-DATABASE_URL: process.env.DATABASE_URL === undefined ? 'postgresql://admin:npg_0leWpxvmgoh9@ep-bold-lab-a4wrf69s-pooler.us-east-1.aws.neon.tech/chamiya?sslmode=require' : process.env.DATABASE_URL,    
+PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,  
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
 };
